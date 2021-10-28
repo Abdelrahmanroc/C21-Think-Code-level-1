a7a0f40c691f3507305b11d164956f63d8df7d8d
Abdelrahman Mohamed <2118018@talnet.nl>
2021-10-28 14:12:43 +0200

    hi

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,11 @@
 #maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
 #elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
 #weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
+
+def favorite_Game (name):
+    print("One of my Game is ", name)
+    
+favorite_Game(" Conquer Online")
+favorite_Game(" Genshin impact")
+favorite_Game(" Clash of clans")
+    
